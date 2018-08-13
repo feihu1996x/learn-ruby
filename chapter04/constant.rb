742d3753f566e2070aa3096d094f2d6ee765ed96
feihu1996x <gsx0312@qq.com>
2018-08-13 15:17:27 +0800

    Ruby中的常量

---
+++ chapter04/constant.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+=begin
+ @file: constant.rb
+ @brief:  Ruby中的常量
+ @author: feihu1996.cn
+ @date:  18-08-13
+ @version: 1.0
+=end
+
+=begin
+1. 常量一旦初始化，其值不可改变
+2. 常量以大写英文字母开头
+3. Ruby 的运行版本( RUBY_VERSION )、\
+    运行平台( RUBY_PLATFORM )、\
+    命令行参数数组( ARGV )等,\
+    都是 Ruby 预定义好的常量
+=end
+
+print("Hello, World!\n")
