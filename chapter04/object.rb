661fb48fd4c7d5da816162b6efa26676667ab584
feihu1996x <gsx0312@qq.com>
2018-08-13 14:29:08 +0800

    Ruby对象初识

---
+++ chapter04/object.rb
@@ -0,0 +1,41 @@
+# encoding: UTF-8
+
+=begin
+ @file: object.rb
+ @brief: Ruby对象初识
+ @author: feihu1996.cn
+ @date:  18-08-13
+ @version: 1.0
+=end
+
+=begin
+在 Ruby 中,表现数据的基本单位称为对象(object)
+常用的对象的有：
+
+一、数值对象
+数字、矩阵、复数、质数、公式
+
+二、字符串对象
+字符序列
+
+三、数组对象、散列对象
+多个数据的集合
+
+四、正则表达式对象
+匹配模式
+
+五、时间对象
+日期时间
+
+六、文件对象
+对文件进行读写操作
+
+七、符号对象
+用于识别方法等名称的标签
+
+八、范围对象(range)
+
+九、异常对象(exception)
+=end
+
+print("Hello, World!\n")
