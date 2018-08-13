db4a76b9d3812d83f8f19f1c00449f958df314c7
feihu1996x <gsx0312@qq.com>
2018-08-13 15:32:52 +0800

    Ruby中的保留字

---
+++ chapter04/reserved.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+
+=begin
+ @file: reserved.rb
+ @brief:  Ruby中的保留字
+ @author: feihu1996.cn
+ @date:  18-08-23
+ @version: 1.0
+=end
+
+=begin
+1. 不能使用保留字作为变量名
+
+2. Ruby 的保留字
+_ _LINE_ _
+_ _ENCODING_ _
+_ _FILE_ _
+BEGIN
+END
+alias
+and
+begin
+break
+case
+class
+def
+defined?
+do
+else
+elsif
+end
+ensure
+false
+for
+if
+in
+module
+next
+nil
+not
+or
+redo
+rescue
+retry
+return
+self
+super
+then
+true
+undef
+unless
+until
+when
+while
+yield
+=end
+
+print("Hello, World!\n")
