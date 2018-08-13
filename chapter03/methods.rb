31034d0b744b5c0916fae57701c5afa558bf2466
feihu1996x <gsx0312@qq.com>
2018-08-13 12:05:53 +0800

    方法的定义

---
+++ chapter03/methods.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+=begin
+ @file: methods.rb
+ @brief:  方法的定义
+ @author: feihu1996.cn
+ @date:  18-08-13
+ @version: 1.0
+=end
+
+# 定义一个名为hello的方法
+def hello
+    puts("Hello,World!")
+end
+
+# 调用 hello方法
+hello()
