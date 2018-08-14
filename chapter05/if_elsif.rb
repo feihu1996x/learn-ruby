8d456f3eb762f33afd3e3a3ebaaa0ebe2d3a4402
feihu1996x <gsx0312@qq.com>
2018-08-15 01:48:45 +0800

    Ruby条件判断之if语句

---
+++ chapter05/if_elsif.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+=begin
+ @file: if_elsif.rb
+ @brief:  Ruby条件判断之if语句
+ @author: feihu1996.cn
+ @date:  18-08-15
+ @version: 1.0
+=end
+
+a = 10
+b = 20
+if a > b then
+    puts("a 比 b 大 ")
+elsif a < b then
+    puts("a 比 b 小 ")
+else
+    puts("a 与 b 相等 ")
+end
