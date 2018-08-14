be72b923ca7d05019730709812851bef237d96d1
feihu1996x <gsx0312@qq.com>
2018-08-15 02:35:43 +0800

    Ruby条件判断之if修饰符与unless修饰符

---
+++ chapter05/condition3.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+
+=begin
+    @file: condition3.rb
+    @brief:  Ruby条件判断之if修饰符与unless修饰符
+    @author: feihu1996.cn
+    @date:  18-08-15
+    @version: 1.0
+=end
+
+a = 10
+b = 20
+
+# if修饰符
+puts("a比b小") if a < b
+
+# if修饰符
+puts("a不比b大") unless a > b
